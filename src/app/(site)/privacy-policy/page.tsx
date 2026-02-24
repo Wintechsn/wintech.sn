dd6cd3bd6070a62fd8f2858e03786ccf06fc1e83
Lindorndiaye <lindoramadou@gmail.com>
2026-02-24 21:53:49 +0000

    fix bug

--- src/app/(site)/privacy-policy/page.tsx
+++ src/app/(site)/privacy-policy/page.tsx
@@ -54,7 +54,6 @@ export default function Page() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </section >
+        </section>
     );
 };
--- src/app/components/home/creative-mind/index.tsx
+++ src/app/components/home/creative-mind/index.tsx
@@ -30,37 +30,38 @@ function CreativeMind({ showTitle = true }: { showTitle?: boolean }) {
     animate: inView ? { y: 0, opacity: 1 } : { y: '10%', opacity: 0 },
     transition: { duration: 0.4, delay: 0.4 + index * 0.3 },
   })
-  // return (
-  //   <section id='team'>
-  //     <div ref={ref} className='2xl:py-20 py-11'>
-  //       <div className='container'>
-  //         <div className='flex flex-col justify-center items-center gap-10 md:gap-20'>
-  //           {showTitle && (
-  //             <div className='max-w-32 text-center'>
-  //               <h2>
-  //                 <TextGenerateEffect words="Découvrez les esprits créatifs à l'origine de" duration={0.5} />
-  //                 <TextGenerateEffect
-  //                   words="notre succès"
-  //                   delay={1}
-  //                   className="italic font-normal instrument-font"
-  //                 />
-  //               </h2>
-  //             </div>
-  //           )}
-  //           <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8'>
-  //             {creativeMindList?.map((item: any, index: any) => {
-  //               return (
-  //                 <motion.div {...bottomAnimation(index)} key={index}>
-  //                   <SingleCreativeMind key={index} creativemind={item} />
-  //                 </motion.div>
-  //               )
-  //             })}
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </section>
-  // )
+
+  return (
+    <section id='team'>
+      <div ref={ref} className='2xl:py-20 py-11'>
+        <div className='container'>
+          <div className='flex flex-col justify-center items-center gap-10 md:gap-20'>
+            {showTitle && (
+              <div className='max-w-32 text-center'>
+                <h2>
+                  <TextGenerateEffect words="Découvrez les esprits créatifs à l'origine de" duration={0.5} />
+                  <TextGenerateEffect
+                    words="notre succès"
+                    delay={1}
+                    className="italic font-normal instrument-font"
+                  />
+                </h2>
+              </div>
+            )}
+            <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-8'>
+              {creativeMindList?.map((item: any, index: any) => {
+                return (
+                  <motion.div {...bottomAnimation(index)} key={index}>
+                    <SingleCreativeMind key={index} creativemind={item} />
+                  </motion.div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default CreativeMind
