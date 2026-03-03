6496e6d6526ca6a5f168d6acdf91e053f8665403
Lindorndiaye <lindoramadou@gmail.com>
2026-03-03 01:28:54 +0000

    MOBILE TESTI

--- src/app/components/home/customer-stories/index.tsx
+++ src/app/components/home/customer-stories/index.tsx
@@ -14,7 +14,7 @@ const TEMOIGNAGES = [
   },
   {
     quote:
-      "« Toujours un plaisir de collaborer avec Wintech. Leur professionnalisme et leur dévouement garantissent un travail digital de grande qualité. Merci pour ce service exceptionnel et innovant. »",
+      "« Toujours un plaisir de collaborer avec Wintech. Leur professionnalisme et leur dévouement garantissent un travail digital de grande qualité. Merci pour ce service exceptionnel »",
     name: "Issa Ndiaye",
     role: "CVO Ovindi",
     image: "/images/home/customerStories/issandiaye.jpg",
@@ -110,7 +110,7 @@ function CustomerStories() {
                         transition={{ duration: 0.6 }}
                         className="flex flex-col gap-6"
                       >
-                        <h4 className="text-white">
+                        <h4 className="text-white text-sm md:text-2xl">
                           {TEMOIGNAGES[activeIndex].quote}
                         </h4>
                         <div className="flex flex-col gap-1">
